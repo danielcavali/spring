34908047932e6926ae7b1c93a83d9c4d18582cba
danielcavali <danielcavali.m@gmail.com>
2022-06-25 10:36:09 -0300

    initial commit

---
+++ src/main/java/spring/example/VendasAplication.java
@@ -0,0 +1,14 @@
+package spring.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasAplication {
+
+    public static void main(String[] args) {
+
+    SpringApplication.run(VendasAplication.class, args);
+
+    }
+}
